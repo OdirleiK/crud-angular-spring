3748bed2c4a67dd97e19f13036f4fd70d40856f6
Odirlei Oliver Kempfer Siqueira Junior <odirlei67777@gmail.com>
2022-10-13 16:06:52 -0300

    adcionando toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { CoursesModule } from './courses/courses.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    CoursesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
